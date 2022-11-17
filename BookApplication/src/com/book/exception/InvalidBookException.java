99c2b99154fe6dc44e6aa9963e9ba41283df51c1
LADEMRUDULAGIT <116784227+LADEMRUDULAGIT@users.noreply.github.com>
2022-11-17 12:12:50 +0530

    Update InvalidBookException.java

--- BookApplication/src/com/book/exception/InvalidBookException.java
+++ BookApplication/src/com/book/exception/InvalidBookException.java
@@ -3,6 +3,7 @@ package com.book.exception;
 @SuppressWarnings("serial")
 public class InvalidBookException extends Exception{
 	public InvalidBookException(String s){
-			System.out.println("Exception is :\n"+s+".....!!!");
+		    super(s);
+			//System.out.println("Exception is :\n"+s+".....!!!");
 	}
 }
